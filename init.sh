4f31e9e85190d55172523ebcab536f665d774eec
friparia <friparia@gmail.com>
2015-01-16 07:39:26 +0000

    add autojump

--- init.sh
+++ init.sh
@@ -2,7 +2,13 @@ cd ~
 wget http://mirrors.yun-idc.com/epel/5/x86_64/epel-release-5-4.noarch.rpm
 rpm -ivh epel-release-5-4.noarch.rpm
 yum install -y zsh
-sed "s/bash/zsh/g" /etc/passwd
-git clone git://github.com/joelthelion/autojump.git
-cd autojump
-./install.py
+sed -i "s/bash/zsh/g" /etc/passwd
+curl -L http://install.ohmyz.sh | sh
+sed -i "s/^ZSH_THEME=.*$/ZSH_THEME=\"geoffgarside\"/g" ~/.zshrc
+sed -i "s/^plugins=.*$/plugins=\(git-flow laravel4 composer git autojump\)/g" ~/.zshrc
+echo "LS_COLORS='di=00;35;36:'" >> ~/.zshrc
+echo "export LS_COLORS" >> ~/.zshrc
+cd ~
+zsh
+
+
---
+++ zsh.sh
@@ -0,0 +1,6 @@
+cd ~
+# git clone git://github.com/joelthelion/autojump.git
+cd autojump
+./install.py
+echo "[[ -s $HOME/.autojump/etc/profile.d/autojump.sh ]] && source $HOME/.autojump/etc/profile.d/autojump.sh" >> ~/.zshrc
+echo "autoload -U compinit && compinit -u" >> ~/.zshrc
