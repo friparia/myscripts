77c44cd8f6b9ea502a2883c59ba025da0a932ad4
gaoyang <friparia@gmail.com>
2015-01-19 17:17:51 +0800

    add something

--- install_vim74_centos65.sh
+++ install_vim74_centos65.sh
@@ -9,7 +9,7 @@ make
 make install
 cd /opt/pkgs
 yum install openssl openssl-devel
-wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
+wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz --no-check-certificate
 tar zxf Python-2.7.9.tgz
 cd Python-2.7.9
 ./configure
@@ -19,12 +19,13 @@ cd /opt/pkgs
 wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
 tar jxvf vim-7.4.tar.bz2
 cd /opt/pkgs
-wget https://bootstrap.pypa.io/get-pip.py
+wget https://bootstrap.pypa.io/get-pip.py --no-check-certificate
 python get-pip.py
 pip install powerline-status
 cd /opt/pkgs
 cd vim74
 yum install gcc make ncurses-devel
+updatedb
 pyconfig=`echo | locate python | grep 'python2.7/config$'`
 ./configure --disable-selinux --enable-multibyte --with-features=huge --with-python-config-dir=$pyconfig --enable-pythoninterp
 make
