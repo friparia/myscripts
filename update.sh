2d3b79f7844e4ecd188a6fe2c8c8363ff7384799
gaoyang <friparia@gmail.com>
2016-04-06 09:32:02 +0800

    add vimrc

--- update.sh
+++ update.sh
@@ -2,4 +2,9 @@ cd ~/myscripts
 git pull 
 cp -f ~/myscripts/.zshrc ~/.zshrc
 cp -f ~/myscripts/.vimrc ~/.vimrc
+d=`date +"%Y-%m-%d_%T"`
+mv ~/.vim ~/.vim.bak.$d
+tar -zxvf vim.tar.gz
+mv ./root/.vim ~/.vim
+vim +BundleInstall +qa
 sh ./update_agnoster_theme.sh
